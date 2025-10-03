eb85a9fb96c2083314465c7f393fbae8f2de4b00
minion779 <157313373+minion779@users.noreply.github.com>
2025-10-03 18:18:17 +0100

    Add files via upload

--- clock/clock.js
+++ clock/clock.js
@@ -1,23 +1,34 @@
 function Showtime() {
     document.getElementById("click").style.display = "none";
-    var date  = new Date();
-    var hour = date.getHours();
-    var min = date.getMinutes();
-    var sec = date.getSeconds();
-    var session = "AM";
-    
-    if (hour == 0) {
-        hour = 12;
-    }
-    if (hour > 12) {
-        hour -= 12;
-        session = "PM"
-    }
-    hour = (hour < 10)?"0" + hour:hour;
-    min = (min < 10)?"0" + min:min;
-    sec = (sec < 10)?"0" + sec:sec;
+    function Upclock() {
+        var date  = new Date();
+        var hour = date.getHours();
+        var min = date.getMinutes();
+        var sec = date.getSeconds();
+        
+        var session = " AM";
+        
+        if (hour == 0) {
+            hour = 12;
+        }
+        if (hour > 12) {
+            hour -= 12;
+            session = " PM"
+        }
+        hour = (hour < 10)?"0" + hour:hour;
+        min = (min < 10)?"0" + min:min;
+        sec = (sec < 10)?"0" + sec:sec;
 
-    var time = hour + ":" + min + ":" + sec + session;
-    document.getElementById("clockDisplay").innerHTML = time;
+        var days = ["Sun","Mon","Tues","Wed","Thu","Fri","Sat"];
+        var months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+        var day = days[date.getDay()];
+        var month = months[date.getMonth()];
+        var yr = date.getFullYear();
+
+        var time = hour + ":" + min + ":" + sec + session + " and the day is " + day + "/" + month + "/" + yr;;
+        document.getElementById("clockDisplay").innerHTML = time;       
+                    
+    }
+    setInterval(Upclock,1000)
 }
-setInterval(Showtime, 1000)
+
